682256e26b8912080d6e40df376bcd77e8ed6ee6
chethanjkulkarni <chethanjkulkarni@gmail.com>
2018-10-10 16:04:31 +0530

    New Login Screen Changes

--- app/src/main/java/in/restroin/restroin/LoginActivity.java
+++ app/src/main/java/in/restroin/restroin/LoginActivity.java
@@ -1,7 +1,11 @@
 package in.restroin.restroin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ImageButton exit_button = (ImageButton) findViewById(R.id.exit_button);
+        exit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(getIntent().getStringExtra("isBackStack") == null){
+                    Toast.makeText(LoginActivity.this, "HI", Toast.LENGTH_SHORT).show();
+                    Intent goToUserFeed = new Intent(LoginActivity.this, UserFeedActivity.class);
+                    startActivity(goToUserFeed);
+                } else {
+                    finish();
+                }
+            }
+        });
     }
 }
